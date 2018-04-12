136f7dad08cf9b1340fc9f9a045fbbc7afa6ef0a
Geisirc <geisianeribeiro8@gmail.com>
2018-04-12 13:47:22 -0300

    Desfeito comentários e logs de tratamento
    Desfeito comentários das datas e adicionado Sysout nos logs de tratamento

--- CadastroKalia/src/br/com/kalia/dao/CadastroDAO.java
+++ CadastroKalia/src/br/com/kalia/dao/CadastroDAO.java
@@ -27,29 +27,17 @@ public class CadastroDAO {
 			" Values (?, ?, ?, ?, ?, ?)") ;
 			
 
-			" (objeto, tipo, requisitante, data_emprestimo, data_devolucao, contato) " + 
+			" (objeto, tipo, requisitante, data_emprestimo, data_devolucao, email) " + 
 			" Values (?, ?, ?, ?, ?, ?)") ;
 
 			int count = 1;
 			stmt.setString(count++, cad.getObjeto());
 			stmt.setString(count++, cad.getTipo());
 			stmt.setString(count++, cad.getRequisitante());
-
-			stmt.setString(count++, cad.getDataEmprestimo());
-			stmt.setString(count++, cad.getDataDevolucao());
-			stmt.setString(count++, cad.getRequisitante());
-			stmt.setString(count++, cad.getEmail());
-			
-			//stmt.setDate(count++, converteData(cad.getDataEmprestimo()));
-			//stmt.setDate(count++, converteData(cad.getDataDevolucao()));
-			
-			
-
 			stmt.setDate(count++, converteData(cad.getDataEmprestimo()));
 			stmt.setDate(count++, converteData(cad.getDataDevolucao()));
-			stmt.setString(count++, cad.getContato());
-
-		
+			stmt.setString(count++, cad.getEmail());
+					
 			if(stmt.executeUpdate() > 0){
 				result = true;
 			}
@@ -68,12 +56,7 @@ public class CadastroDAO {
 		return new java.sql.Date(dataUtil.getTime());
 	}
 
-	
-	public static String dataForSQL(String Data){
-    	return Data.substring(8, 10) + "-" + Data.substring(5, 7) + "-" + Data.substring(0, 4);
-    }
-
-	
+		
 	public List<Cadastro> buscar(String pesquisa) throws ClassNotFoundException, ParseException{
 		
 		Connection conexao = ConnectionFactory.abrirConexao();
@@ -88,51 +71,49 @@ public class CadastroDAO {
                 count = res.getInt(1);
                 
                 SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd"); 
-                //Date dataEmprestimo = (Date) formato.parse(res.getString("data_emprestimo"));
-                //Date dataDevolucao = (Date) formato.parse(res.getString("data_devolucao")); 
+                Date dataEmprestimo = (Date) formato.parse(res.getString("data_emprestimo"));
+                Date dataDevolucao = (Date) formato.parse(res.getString("data_devolucao")); 
                 
                                
                 Cadastro pp1 = new Cadastro();
-                //pp1.getIdRequisitante(Integer.parseInt(res.getString("idRequisitante"));
+                pp1.getIdRequisitante(Integer.parseInt(res.getString("idRequisitante"));
                 pp1.setObjeto(res.getString("objeto"));
                 pp1.setTipo(res.getString("tipo"));
 
                 pp1.setRequisitante(res.getString("requisitante"));
-                pp1.setDataEmprestimo(res.getString("data_emprestimo"));
-                pp1.setDataDevolucao(res.getString("dataDevolucao"));
-                pp1.setEmail(res.getString("email"));
-
-                pp1.setRequisitante(res.getString("requisitante"));               
-                pp1.setDataDevolucao(dataDevolucao);
-                pp1.setContato(res.getString("contato"));
-
-                
-                ll.add(pp1);                
+                pp1.setDate(dataEmprestimo);
+		pp1.setDate(dataDevolucao);
+                pp1.setEmail(res.getString("email"));                       
+                               
+                ll.add(pp1);
+		return pp1;
             }
 			
-			if(count > 0){
-				return ll;
-			}
+		if(count > 0){
+			return ll;
+		}
 
 			st.close();	
 		} catch (SQLException e) {
-			System.out.println(e);
+			System.out.println(e + "Erro ao buscar registros na pesquisa");			
 		}finally{
 			ConnectionFactory.fecharConexao();
+			System.out.println("Fechando conexão da busca");	
 		}
 		
-		return null;
+		return pp1;
 	}
 	
 
-	//Método utilizado para modificar dados dos usuário. Esse método
-	//recebe os dados já alterados e envia pro banco.
+	/*
+	Método utilizado para modificar dados dos usuário. Esse método
+	recebe os dados já alterados e envia pro banco.
+	*/
 	public static Cadastro editaUsuario(Cadastro usuario) throws ClassNotFoundException{
 		Connection conexao = ConnectionFactory.abrirConexao();
 		PreparedStatement ps=null;
 		try {
-			//os dados contidos na query devem estar escritos igual como
-			//os dados do banco
+			//os dados contidos na query devem estar escritos igual como os dados do banco
 			String query="UPDATE cadastro SET objeto=?,tipo=?,requisitante=?,data_emprestimo=?,data_devolucao=?,email=? WHERE idRequisitante=?";
 			ps=conexao.prepareStatement(query);	
 			
@@ -140,15 +121,19 @@ public class CadastroDAO {
 			ps.setString(count++, usuario.getObjeto());;
 			ps.setString(count++, usuario.getTipo());;
 			ps.setString(count++, usuario.getRequisitante());;
-			ps.setString(count++, usuario.getDataEmprestimo());;
-			ps.setString(count++, usuario.getDataDevolucao());;
+			
+			ps.setDate(count++, converteData(cad.getDataEmprestimo()));
+			ps.setDate(count++, converteData(cad.getDataDevolucao()));
+			
 			ps.setString(count++, usuario.getEmail());;
 			ps.executeUpdate();
 			
+			System.out.println("Editado cadastro do usuário");
+			
 		} catch (Exception e) {
-				System.out.println(e);
+			System.out.println(e + "Erro ao editar cadastro do usuário");
 		}
-		return usuario;		
+		return null;		
 	}
 
 	
@@ -156,18 +141,19 @@ public class CadastroDAO {
 	//Método utilizado para excluir usuarios
 	public static Cadastro excluirUsuario(String id) throws ClassNotFoundException {
 		
-		int cod_usuario = Integer.parseInt(id);
+		int codUsuario = Integer.parseInt(id);
 		Connection conexao = ConnectionFactory.abrirConexao();
 		String query = "delete from usuario where id_usuario=?";
 		try {
 			PreparedStatement ps = conexao.prepareStatement(query);
 			ps.setInt(1, cod_usuario);
 			ps.executeUpdate();
-						
+			System.out.println("Excluindo cadastro do usuário" + codUsuario);			
 		} catch (SQLException e) {
-			e.printStackTrace();
+			System.out.println("Erro ao excluir cadastro do usuário" + e.printStackTrace();
 		}finally{
 			ConnectionFactory.fecharConexao();
+			System.out.println("Fechando conexão de exclusão");
 		}
 		return null;
 		
@@ -186,28 +172,30 @@ public class CadastroDAO {
                 count = res.getInt(1);  
                 
                 SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd"); 
-        		//Date dataEmprestimo = (Date) formato.parse(res.getString("data_emprestimo"));
-        		//Date dataDevolucao = (Date) formato.parse(res.getString("data_devolucao")); 
+        	Date dataEmprestimo = (Date) formato.parse(res.getString("data_emprestimo"));
+        	Date dataDevolucao = (Date) formato.parse(res.getString("data_devolucao")); 
         		
-                //pp1.setIdCadastro(Integer.parseInt(res.getString("idCadastro")));
+                pp1.setIdCadastro(Integer.parseInt(res.getString("idCadastro")));
                 pp1.setObjeto(res.getString("objeto"));                
                 pp1.setTipo(res.getString("tipo"));
                 pp1.setRequisitante(res.getString("requisitante"));
-                pp1.setDataEmprestimo(res.getString("data_emprestimo"));
-                pp1.setDataDevolucao(res.getString("data_devolucao"));
+                pp1.setDataEmprestimo(dataEmprestimo);
+                pp1.setDataDevolucao(dataDevolucao);
                 pp1.setEmail(res.getString("email"));
                 
-            }
+               }
 			
-			if(count > 0){
-				return pp1;
-			}
+		f(count > 0){
+			return pp1;
+		}
 
 			st.close();	
 		} catch (SQLException e) {
-			System.out.println(e);
+			System.out.println(e + "Erro na busca por id");
+			
 		}finally{
 			ConnectionFactory.fecharConexao();
+			System.out.println("Fechando conexão da busca por id");
 		}
 		
 		return null;
